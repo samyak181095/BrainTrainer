1969a57f6017928b793022b64e33f3a9b8baac3f
Samyak Jain <jain.samyak181095@gmail.com>
2017-12-22 23:02:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/sammy/braintrainer/MainActivity.java
+++ app/src/main/java/com/sammy/braintrainer/MainActivity.java
@@ -3,7 +3,6 @@ package com.sammy.braintrainer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
